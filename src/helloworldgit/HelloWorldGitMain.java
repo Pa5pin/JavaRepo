0b93d3ad77fc510d51e8cc3dc1a63d040e71f0ef
Paul Aspin <paulaspin@pauls-imac.home>
2020-06-25 11:43:09 +0100

    added java files'

---
+++ src/helloworldgit/HelloWorldGitMain.java
@@ -0,0 +1,15 @@
+package helloWorld;
+
+public class HelloWorld {
+	
+	public static void printMessage() {
+		String message = "Hello World";
+		System.out.println(message);
+	}
+	
+	public static void main (String[]args) {
+		
+		printMessage();
+	}
+
+}
